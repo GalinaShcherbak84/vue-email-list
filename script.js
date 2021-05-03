9548ff5680bf9f07814f3e72db622ef0e8b6fc9e
GalinaSherbak84 <galinasherbak@gmail.com>
2021-05-03 18:48:04 +0200

    10 mail

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const app = new Vue({
     el: '#app',
     data:{
-
+        mails:[],
+        
     },
-    created(){
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(result=>{
-            console.log(result)
-            console.log(result.data.response)
-        })
-        .catch(error=>{
+    mounted(){
+        for(let i = 0; i<10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(result=>{
+                //console.log(result)
+                //console.log(result.data.response)
+                this.mails.push(result.data.response)
+            })
+            .catch(error=>{
             console.log('Errore',error);
-        })
-    }
+            })
+        }
+    },
 });
